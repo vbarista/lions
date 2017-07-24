370b4ed5afeae594c2bcac7dd21c46ee68e80d9f
Fujiki Kazuhisa <deep.bonds@gmail.com>
2017-07-24 13:39:28 +0900

    bootstrapの適用しナビバーを作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def admin_user?
+    current_user.try(:admin_flg?)
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,7 @@ module Lions
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.i18n.default_locale = :ja
   end
 end
