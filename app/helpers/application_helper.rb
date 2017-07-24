76e6fcdecbd0d7d9da3d7e75eabadacf1fcf9697
Fujiki Kazuhisa <deep.bonds@gmail.com>
2017-07-24 17:57:11 +0900

    イベント一覧画面のデザイン調整

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def admin_user?
     current_user.try(:admin_flg?)
   end
+
+  def edit_user?
+    current_user.try(:admin_flg?) || current_user.try(:edit_flg?)
+  end
 end
