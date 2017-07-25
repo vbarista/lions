f2089de2c7dc7f0adbb0926c887ebec099ba7132
Fujiki Kazuhisa <deep.bonds@gmail.com>
2017-07-25 15:23:44 +0900

    ユーザーテーブルにusernameを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170724014251) do
+ActiveRecord::Schema.define(version: 20170725062158) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -35,8 +35,10 @@ ActiveRecord::Schema.define(version: 20170724014251) do
     t.datetime "updated_at", null: false
     t.boolean "admin_flg"
     t.boolean "edit_flg"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
