40aa2350c14036fb6c5a8f544f7163a52c13f0cb
Fujiki Kazuhisa <deep.bonds@gmail.com>
2017-07-25 17:09:27 +0900

    名前でもemailでもログインできるように変更

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -11,12 +11,14 @@ class Users::SessionsController < Devise::SessionsController
     #self.resource = warden.authenticate!(auth_options)
 
     #emailだけでログインできるように変更
-    self.resource = User.where(email: params[:user]['email']).first
-
-    set_flash_message(:notice, :signed_in) if is_flashing_format?
-    sign_in(resource_name, resource)
-    yield resource if block_given?
-    respond_with resource, location: after_sign_in_path_for(resource)
+    if self.resource = User.where(["lower(username) = :value OR lower(email) = :value", { :value => params[:user]['login'] }]).first
+      set_flash_message(:notice, :signed_in) if is_flashing_format?
+      sign_in(resource_name, resource)
+      yield resource if block_given?
+      respond_with resource, location: after_sign_in_path_for(resource)
+    else
+      redirect_to new_user_session_path, notice: "ログインエラー"
+    end
   end
 
   def destroy
