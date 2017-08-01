df17dc3d6df68d86b66e36d3e010518ae7f048d2
Fujiki Kazuhisa <deep.bonds@gmail.com>
2017-08-01 17:28:52 +0900

    グループ機能を追加(委員会)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170727051809) do
+ActiveRecord::Schema.define(version: 20170801082409) do
 
   create_table "confirmations", force: :cascade do |t|
     t.integer "user_id"
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 20170727051809) do
     t.datetime "datetime"
     t.string "contents"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "type"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
