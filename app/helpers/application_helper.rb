7c0af10b2f51bed3e90f02ff5f0cef67fbc9aa10
Fujiki Kazuhisa <deep.bonds@gmail.com>
2017-08-01 13:33:03 +0900

    イベントの編集は作成者と管理者だけに限定する

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,9 @@ module ApplicationHelper
   def edit_user?
     current_user.try(:admin_flg?) || current_user.try(:edit_flg?)
   end
+  
+  # イベントの作成者 or 管理者がイベント情報を編集可能
+  def event_edit?(event)
+    current_user.try(:admin_flg?) || event.try(:user) == current_user
+  end
 end
