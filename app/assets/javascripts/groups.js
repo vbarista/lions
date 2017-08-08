711964706ea366ca9cbf92aa1324ca103e015fa0
Fujiki Kazuhisa <deep.bonds@gmail.com>
2017-08-08 10:52:12 +0900

    デザイン調整

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -1,33 +1,8 @@
 $(function() {
-  // 所属メンバー選択制御（checkbox）
-  var fn_checkbox = function() {
-    var $label = $(this);
-    var $checkbox = $label.children(':checkbox');
-    
-    if ($checkbox.prop('checked') === true)  {
-      $label.addClass('btn-primary').removeClass('active');
-    } else {
-      $label.removeClass('btn-primary').removeClass('active')
-    }
-  };
-
-  // 所属メンバー選択制御（checkbox）
-  $('.checkbox_label').on('click', function(){
-    var $label = $(this);
-    var $checkbox = $label.children(':checkbox');
-    $checkbox.prop('checked', !$checkbox.prop('checked'));
-    if ($checkbox.prop('checked') === true)  {
-      $label.addClass('btn-primary').removeClass('active');
-    } else {
-      $label.removeClass('btn-primary').removeClass('active');
-    }
-  });
-  $('.checkbox_label').each(fn_checkbox);
-
   // 委員会選択制御（radio）
   var fn_radio = function() {
-    $('.radio_label').removeClass('btn-primary').removeClass('active');
-    $(':radio:checked').parent('.radio_label').addClass('btn-primary').addClass('active');
+    $('.radio_label').removeClass('btn-success').removeClass('active');
+    $(':radio:checked').parent('.radio_label').addClass('btn-success').removeClass('active');
   };
   $('.radio_label').on('click', function(){
     var $radio = $(this).children(':radio');
@@ -35,5 +10,28 @@ $(function() {
     fn_radio();
   });
   fn_radio();
+  
+  // 所属メンバー選択制御（checkbox）
+  var fn_checkbox = function() {
+    var $label = $(this);
+    var $checkbox = $label.children(':checkbox');
+    
+    if ($checkbox.prop('checked') === true)  {
+      $label.addClass('btn-success').removeClass('active');
+    } else {
+      $label.removeClass('btn-success').removeClass('active');
+    }
+  };
 
+  $('.checkbox_label').on('click', function(){
+    var $label = $(this);
+    var $checkbox = $label.children(':checkbox');
+    $checkbox.prop('checked', !$checkbox.prop('checked'));
+    if ($checkbox.prop('checked') === true)  {
+      $label.addClass('btn-success').removeClass('active');
+    } else {
+      $label.removeClass('btn-success').removeClass('active');
+    }
+  });
+  $('.checkbox_label').each(fn_checkbox);
 });
