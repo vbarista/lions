8cf0951fc4cf6dae3dfe46a8cb8642cf5588bb50
Fujiki Kazuhisa <deep.bonds@gmail.com>
2017-08-08 11:06:36 +0900

    参加・不参加・未回答パネルの表示制御

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,26 @@
 module EventsHelper
+  # 参加ユーザー
+  def join_users(event)
+    Confirmation.includes(:user).where(event: event, onoff: 1).select(user: [:username]).pluck(:username).uniq
+  end
+  
+  # 不参加ユーザー
+  def not_join_users(event)
+    Confirmation.includes(:user).where(event: event, onoff: 2).select(user: [:username]).pluck(:username).uniq
+  end
+  
+  # 未回答ユーザー
+  def no_sign_users(event)
+    no_sign_users = []
+    groups ||= event.groups
+    groups.each do |group|
+      no_sign_users.concat (
+        group.users
+             .where.not(id: event.confirmations.where(onoff: 1).or(event.confirmations.where(onoff: 2))
+             .select(:username)
+             .pluck(:username))
+      )
+    end
+    no_sign_users.uniq
+  end
 end
--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,17 +1,2 @@
 module GroupsHelper
-  # 参加ユーザー
-  def join_users(event)
-    Confirmation.includes(:user).where(event: event, onoff: 1).select(user: [:username]).pluck(:username).uniq
-  end
-  
-  # 不参加ユーザー
-  def not_join_users(event)
-    not_join_users ||=Confirmation.includes(:user).where(event: event, onoff: 2).select(user: [:username]).pluck(:username).uniq
-  end
-  
-  # 未回答ユーザー
-  def no_sign_users(event)
-    no_sign_users = event.groups.first.users.where.not(id: event.confirmations.where(onoff: 1).or(event.confirmations.where(onoff: 2)).select(:user_id).pluck(:user_id))
-    no_sign_users.select(user: [:username]).pluck(:username).uniq
-  end
 end
